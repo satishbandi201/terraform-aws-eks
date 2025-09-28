8a4277d2a3e6c1f5894d46911fe0cfaea96097c2
satishbandi201 <bandisatish20@gmail.com>
2025-09-28 17:20:57 +0530

    teks

---
+++ 80-eks/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z10145872BEYXWGCKYUZ9"
+}
+
+variable "zone_name" {
+    default = "satish84s.site"
+}
