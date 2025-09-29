a838822a030a867f313c1e018dd8f9fe296d2902
satishbandi201 <bandisatish20@gmail.com>
2025-09-29 20:34:41 +0530

    teks

--- 70-frontend-alb/data.tf
+++ 70-frontend-alb/data.tf
@@ -6,8 +6,8 @@ data "aws_ssm_parameter" "public_subnet_ids" {
   name = "/${var.project}/${var.environment}/public_subnet_ids"
 }
 
-data "aws_ssm_parameter" "frontend_alb_sg_id" {
-  name = "/${var.project}/${var.environment}/frontend_alb_sg_id"
+data "aws_ssm_parameter" "ingress_alb_sg_id" {
+  name = "/${var.project}/${var.environment}/ingress_alb_sg_id"
 }
 
 data "aws_ssm_parameter" "acm_certificate_arn" {
